55dd7403183d5a7566c82e2369a9223b6392fe46
Matthew Carney <matthewcarney64@gmail.com>
2017-11-08 12:15:18 +0000

    Another tweak

--- install_base_packages.bash
+++ install_base_packages.bash
@@ -8,7 +8,7 @@ sudo apt-get install -y nano git curl software-properties-common php-pear \
 # Install php5
 expect -c "
 spawn sudo add-apt-repository ppa:ondrej/php
-expect -nocase \"Press [ENTER] to continue or ctrl-c to cancel adding it\" {send \"\r\"; interact}
+expect -nocase \"*to continue or ctrl-c to cancel adding it\" {send \"\r\"; interact}
 "
 #sudo add-apt-repository ppa:ondrej/php
 sudo apt-get update -y
@@ -18,12 +18,13 @@ sudo apt-get install -y php5.6-fpm php5.6-curl php5.6-mcrypt php5.6-gd \
 # Install nginx
 expect -c "
 spawn sudo add-apt-repository ppa:ondrej/nginx
-expect -nocase \"Press [ENTER] to continue or ctrl-c to cancel adding it\" {send \"\r\"; interact}
+expect -nocase \"*to continue or ctrl-c to cancel adding it\" {send \"\r\"; interact}
 "
 #sudo add-apt-repository ppa:ondrej/nginx #'deb http://nginx.org/packages/ubuntu/ trusty nginx'
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $key
 sudo apt-get update -y
 sudo apt-get install -y nginx
+sudo apt-get clean
 
 # Move nginx cnfig
 sudo mv vhost.conf /etc/nginx/conf.d/default.conf
